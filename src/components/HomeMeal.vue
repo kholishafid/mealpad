9954c4e3d924962663f7f50ac4ed52c7ce22beab
hafidnurkholis <itsmaehere@gmail.com>
2022-12-01 14:00:00 +0700

    penyesuaian halaman home

--- src/components/HomeMeal.vue
+++ src/components/HomeMeal.vue
@@ -1,55 +1,33 @@
 <template>
-    <div class="border border-paragraph border-b-0 mx-1 py-3 bg-tertiary h-14 box-border">
-        <h1 class="text-2xl text-center text-white">Wanna try something new?</h1>
+    <div class="border border-paragraph border-b-0 py-3 bg-slate-200 h-14 box-border">
+        <h1 class="text-2xl text-center ">How about this meal?</h1>
     </div>
-    <div class=" overflow-hidden border border-paragraph rounded-sm bg-slate-200 flex mx-1 text-paragraph">
-        <img :src="randomRecipe.strMealThumb" :alt="randomRecipe.strMeal" class="w-[150px] object-cover">
-        <div class="p-6 border-paragraph border-l box-border">
-            <p class="text-2xl font-bold mb-2">{{ randomRecipe.strMeal }}</p>
+    <div class=" overflow-hidden border border-paragraph rounded-sm bg-white text-paragraph">
+        <img :src="randomRecipe.strMealThumb" :alt="randomRecipe.strMeal">
+        <div class="p-6 border-paragraph border-t box-border">
+            <p class="text-xl font-bold mb-2">{{ randomRecipe.strMeal }}</p>
             <div>
                 <span class="mr-2">#{{ randomRecipe.strCategory }}</span>
                 <span>#{{ randomRecipe.strArea }}</span>
             </div>
+            <button class="border rounded-md px-2 py-0.5 bg-slate-200 mt-2 border-paragraph "
+                @click="router.push({ path: `recipe/${randomRecipe.idMeal}` })">Recipe</button>
         </div>
-    </div>
-    <div class="border border-t-0 border-paragraph mx-1 p-5">
-        <div class="flex">
-            <div class="mr-10">
-                <h4 class="mb-5">Ingridients</h4>
-                <ul v-for="item in 20" :key="item.id">
-                    <li v-if="randomRecipe[`strIngredient${item}`]">
-                        {{ item }}. {{ randomRecipe[`strIngredient${item}`] }}
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <h4 class="mb-5">Measure</h4>
-                <ul v-for="item in 20" :key="item.id">
-                    <li v-if="randomRecipe[`strMeasure${item}`]">
-                        {{ randomRecipe[`strMeasure${item}`] }}
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="mt-5 border-t border-paragraph py-5">
-            <h4>Intructions</h4>
-            <pre class="mt-5  whitespace-pre-wrap font-Signika">{{ randomRecipe.strInstructions }}</pre>
-        </div>
-    </div>
-    <div class="border border-paragraph border-b-0 mx-1 py-3 bg-tertiary h-14 box-border">
     </div>
 </template>
 
 <script setup>
 import axios from "axios"
 import { ref } from 'vue';
+import { useRouter } from "vue-router";
 
+const router = useRouter()
 const props = defineProps(['meal'])
 
 const randomRecipe = ref(null)
 
 await axios({
-    method: 'post',
+    method: 'get',
     url: 'https://www.themealdb.com/api/json/v1/1/random.php',
 }).then((res) => {
     randomRecipe.value = res.data.meals[0]
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,24 +4,28 @@ import MealCard from '../components/HomeMeal.vue'
 </script>
 
 <template>
-  <main class="w-screen mt-20 flex">
-    <div class="flex flex-col items-center w-full h-screen">
+  <main class="w-screen">
+    <div class="border-x border-paragraph">
+      <div class="flex flex-col items-center w-full border-paragraph py-20">
+        <div>
+          <img class="max-w-[300px]" src="@/assets/vegan-recipe.png" :alt="lets - cook - illust">
+        </div>
+        <div class="text-center my-4">
+          <h1 class="text-3xl text-paragraph">Lets Cooking With Me!</h1>
+        </div>
+        <div class="mt-2">
+          <router-link to="/search" class="px-4 py-2 bg-button text-paragraph rounded border border-paragraph block">
+            Search
+            Recipes</router-link>
+        </div>
+      </div>
       <div>
-        <img class="max-w-[250px]" src="@/assets/vegan-recipe.png" :alt="lets - cook - illust">
-      </div>
-      <div class="text-center my-4">
-        <h1 class="text-3xl text-paragraph">Lets Cooking With Me!</h1>
-      </div>
-      <div class="mt-2">
-        <router-link to="/search" class="px-4 py-2 bg-button text-paragraph rounded border border-paragraph block">
-          Search
-          Recipes</router-link>
-      </div>
-      <div class="my-40">
         <Suspense>
           <MealCard />
           <template #fallback>
-            <p>loading..</p>
+            <div>
+              <div class="border border-paragraph border-b-0 py-3 bg-tertiary h-14 box-border"></div>
+            </div>
           </template>
         </Suspense>
       </div>
