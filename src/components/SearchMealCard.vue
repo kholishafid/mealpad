3d87cd74a8a7640a8fad63fce94d3cd8c8ddb6e2
hafidnurkholis <itsmaehere@gmail.com>
2022-12-03 17:46:33 +0700

    search view

---
+++ src/components/SearchMealCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="">
+        <div>
+            <h3>{{ meal.strMeal }}</h3>
+            <div class="text-xs mt-1 flex justify-between items-end">
+                <div>
+                    <p class="mr-2"># {{ meal.strCategory }}</p>
+                    <p># {{ meal.strArea }}</p>
+                </div>
+                <a class="underline text-highlight" @click="router.push({ path: `recipe/${meal.idMeal}` })">Recipe</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+
+const router = useRouter()
+const props = defineProps(['meal'])
+</script>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,5 +1,140 @@
 <template>
-    <div>
-        Search
+    <div class="p-5 border-x border-paragraph">
+        <div class="flex gap-1">
+            <input type="text" name="" id=""
+                class="px-3 py-1 rounded-md outline-none border border-paragraph bg-slate-100 w-full"
+                v-model="searchQuery">
+            <button class=" w-12 h-10 border border-paragraph rounded-md"><span
+                    class="material-icons-outlined flex items-center justify-center">
+                    search
+                </span></button>
+        </div>
+    </div>
+    <div class="flex flex-wrap px-5 justify-center mb-5">
+        <h3 class="mr-3">Show this category : </h3>
+        <div v-for="item in mealCategory" :key="item.id">
+            <span class="mr-3 underline">{{ item.strCategory }}</span>
+        </div>
+    </div>
+    <div class="flex flex-wrap px-5 justify-center mb-5">
+        <h3 class="mr-3">Show this Area : </h3>
+        <div v-for="item in mealArea" :key="item.id">
+            <span class="mr-3 underline">{{ item.strArea }}</span>
+        </div>
+    </div>
+    <div class="px-5 py-3 border-t border-paragraph bg-slate-200">
+        <h3>Search Result</h3>
+    </div>
+    <div class="grid grid-cols-2 border-t border-l border-paragraph ">
+        <div v-for="meal in data" :key="meal.id" class="p-4 box-border bg-slate-50 border-b border-r border-paragraph">
+            <SearchMealCard :meal="meal" />
+        </div>
     </div>
 </template>
+
+<script setup>
+import axios from 'axios';
+import { ref, watchEffect } from 'vue';
+import SearchMealCard from '../components/SearchMealCard.vue';
+
+const data = ref(null)
+const category = ref(null)
+
+const searchQuery = ref(null)
+
+watchEffect(async () => {
+    await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery.value}`).then(res => {
+        data.value = res.data.meals
+        return
+    })
+    console.log(data.value)
+})
+
+const mealCategory = [
+    { "strCategory": "Beef" }, { "strCategory": "Breakfast" }, { "strCategory": "Chicken" }, { "strCategory": "Dessert" }, { "strCategory": "Goat" }, { "strCategory": "Lamb" }, { "strCategory": "Miscellaneous" }, { "strCategory": "Pasta" }, { "strCategory": "Pork" }, { "strCategory": "Seafood" }, { "strCategory": "Side" }, { "strCategory": "Starter" }, { "strCategory": "Vegan" }, { "strCategory": "Vegetarian" }
+]
+
+const mealArea = [
+    {
+        "strArea": "American"
+    },
+    {
+        "strArea": "British"
+    },
+    {
+        "strArea": "Canadian"
+    },
+    {
+        "strArea": "Chinese"
+    },
+    {
+        "strArea": "Croatian"
+    },
+    {
+        "strArea": "Dutch"
+    },
+    {
+        "strArea": "Egyptian"
+    },
+    {
+        "strArea": "French"
+    },
+    {
+        "strArea": "Greek"
+    },
+    {
+        "strArea": "Indian"
+    },
+    {
+        "strArea": "Irish"
+    },
+    {
+        "strArea": "Italian"
+    },
+    {
+        "strArea": "Jamaican"
+    },
+    {
+        "strArea": "Japanese"
+    },
+    {
+        "strArea": "Kenyan"
+    },
+    {
+        "strArea": "Malaysian"
+    },
+    {
+        "strArea": "Mexican"
+    },
+    {
+        "strArea": "Moroccan"
+    },
+    {
+        "strArea": "Polish"
+    },
+    {
+        "strArea": "Portuguese"
+    },
+    {
+        "strArea": "Russian"
+    },
+    {
+        "strArea": "Spanish"
+    },
+    {
+        "strArea": "Thai"
+    },
+    {
+        "strArea": "Tunisian"
+    },
+    {
+        "strArea": "Turkish"
+    },
+    {
+        "strArea": "Unknown"
+    },
+    {
+        "strArea": "Vietnamese"
+    }
+]
+</script>
