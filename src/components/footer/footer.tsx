5c2cffa8d340bbff3bdfdf52158ac9c22dabcb19
hafidnurkholis <itsmaehere@gmail.com>
2023-05-25 09:13:51 +0700

    style: footer layout

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,14 @@
+import RunningText from "../running-text";
 import RunningItem from "./running-item";
 
 const Footer = () => {
   return (
-    <footer className='py-16 bg-emerald mt-8'>
-      <div className="container mx-auto px-10">
+    <footer className='bg-emerald'>
+
+      <div className="container mx-auto px-10 py-8">
         <RunningItem />
       </div>
+
     </footer >
   );
 }
