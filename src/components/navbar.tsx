350a1bfeca633ac4d443a81b3d5f27e2d2dd8a16
hafidnurkholis <itsmaehere@gmail.com>
2023-05-29 05:31:24 +0700

    feat: nav link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,28 +1,49 @@
 import { useState } from 'react';
-import MenuIcon from '../assets/icon/menu-1.svg'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 const Navbar = () => {
-  const [sidebarActive, setSidebarActive] = useState();
+  const [sidebarActive, setSidebarActive] = useState<boolean>(false);
 
+  const { pathname } = useLocation()
+
+  const routerActiveC = 'py-1 px-2  bg-black text-white w-full'
   return (
-    <nav className='p-6 flex justify-between sticky top-0 bg-white z-1 '>
+    <nav className='p-4 flex justify-between sticky top-0 bg-slate-1 z-1 '>
       <Link to={'/'} className='flex items-center gap-2 cursor-pointer'>
         <img
-          className='h-8'
+          className='h-8 hidden md:block'
           src='/bowl.png'
           alt='Meal Pad Logo'
         />
         <span className='font-semibold font-sans text-xl'>Meal Pad</span>
       </Link>
       <div className='flex items-center'>
-        <img src={MenuIcon} alt="mealpad menu" />
-        {/* <ul className='absolute h-screen w-screen bg-amber top-0 right-0'>
+        <img
+          className='h-9 bg-slate-2 rounded cursor-pointer border border-black p-0.5 md:hidden'
+          src='/bowl.png'
+          alt='Meal Pad Logo'
+          onClick={() => setSidebarActive(true)}
+        />
+        <ul
+          className={'md:(flex gap-4 font-sans font-medium) lg:text-lg ' + (sidebarActive === true ? 'fixed inset-y-0 left-1/3 right-0 bg-slate-1 p-8 text-3xl flex flex-col gap-4 items-end' : 'hidden')}
+        >
           <li>
-            <Link to={'/'}>Home</Link>
-            <Link to={'/Search'}>Search</Link>
-            <Link to={'/Search'}>Favorites</Link>
+            <span
+              className='ml-auto block w-fit md:hidden cursor-pointer'
+              onClick={() => setSidebarActive(false)}
+            >
+              &#10005;
+            </span>
           </li>
-        </ul> */}
+          <li>
+            <Link to={'/'} className={pathname === '/' ? routerActiveC : ''}>Home</Link>
+          </li>
+          <li>
+            <Link to={'/search'} className={pathname === '/search' ? routerActiveC : ''}>Search</Link>
+          </li>
+          <li>
+            {/* <Link to={'/favorites'} className={pathname === '/favorites' ? routerActiveC : ''}>Favorites</Link> */}
+          </li>
+        </ul>
       </div>
     </nav>
   );
